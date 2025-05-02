26527ad5cd104e07cd6f191faa274a41efd3977f
catarinaflores <catarina10flores@gmail.com>
2025-05-02 16:31:23 +0100

    C++ added
    player movement

---
+++ Source/FirstGame/Scripts/CPPCharacter.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPCharacter.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ACPPCharacter::ACPPCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Player Camera"));
+	Camera->SetupAttachment(RootComponent);
+	Camera->bUsePawnControlRotation = true;
+}
+
+// Called when the game starts or when spawned
+void ACPPCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPPCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACPPCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAxis("Move_Forward", this, &ACPPCharacter::Move_Forward);
+	PlayerInputComponent->BindAxis("Move_Right", this, &ACPPCharacter::Move_Right);
+
+	PlayerInputComponent->BindAxis("Turn", this, &ACPPCharacter::Turn);
+}
+
+void ACPPCharacter::Move_Forward(float InputValue)
+{
+	FVector ForwardDirection = GetActorForwardVector();
+	AddMovementInput(ForwardDirection, InputValue);
+}
+
+void ACPPCharacter::Move_Right(float InputValue)
+{
+	FVector RightDirection = GetActorRightVector();
+	AddMovementInput(RightDirection, InputValue);
+}
+
+void ACPPCharacter::Turn(float InputValue)
+{
+	AddControllerYawInput(InputValue);
+}
+
